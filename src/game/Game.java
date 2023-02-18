8bf20409fd421c81871438a490a5b3da4294a292
unknown <ahmad.ib04@gmail.com>
2023-02-18 19:06:49 +0100

    refactorize the Game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -24,83 +24,96 @@ public class Game {
 			Direction direction = point.getDirection();
 			switch (command) {
 			case 1:
-				switch (direction) {
-				case NORTH:
-					point.setY(point.getY() - 1);
-					break;
-				case SOUTH:
-					point.setY(point.getY() + 1);
-					break;
-				case EAST:
-					point.setX(point.getX() + 1);
-					break;
-				case WEST:
-					point.setX(point.getX() - 1);
-					break;
-				}
-				if(!checkIfPointIsOnTheTable()) {
-					return;
-				}
+				movePoint(1, direction);
 				break;
 			case 2:
-				switch (direction) {
-				case NORTH:
-					point.setY(point.getY() + 1);
-					break;
-				case SOUTH:
-					point.setY(point.getY() - 1);
-					break;
-				case EAST:
-					point.setX(point.getX() - 1);
-					break;
-				case WEST:
-					point.setX(point.getX() + 1);
-					break;
-				}
-				if(!checkIfPointIsOnTheTable()) {
-					return;
-				}
+				movePoint(2, direction);
 				break;
 			case 3:
-				switch (direction) {
-				case NORTH:
-					point.setDirection(Direction.EAST);
-					break;
-				case SOUTH:
-					point.setDirection(Direction.WEST);
-					break;
-				case EAST:
-					point.setDirection(Direction.SOUTH);
-					break;
-				case WEST:
-					point.setDirection(Direction.NORTH);
-					break;
-				}
+				moveDirection(3, direction);
 				break;
 			case 4:
-				switch (direction) {
-				case NORTH:
-					point.setDirection(Direction.WEST);
-					break;
-				case SOUTH:
-					point.setDirection(Direction.EAST);
-					break;
-				case EAST:
-					point.setDirection(Direction.NORTH);
-					break;
-				case WEST:
-					point.setDirection(Direction.SOUTH);
-					break;
-				}
+				moveDirection(4, direction);
 				break;
 			case 0:
 				break;
 			}
+			if (!checkIfPointIsOnTheTable()) {
+				return;
+			}
 		}
 	}
-	
+
+	private void movePoint(int command, Direction direction) {
+		if (command == 1) {
+			switch (direction) {
+			case NORTH:
+				point.setY(point.getY() - 1);
+				break;
+			case SOUTH:
+				point.setY(point.getY() + 1);
+				break;
+			case EAST:
+				point.setX(point.getX() + 1);
+				break;
+			case WEST:
+				point.setX(point.getX() - 1);
+				break;
+			}
+		} else if (command == 2) {
+			switch (direction) {
+			case NORTH:
+				point.setY(point.getY() + 1);
+				break;
+			case SOUTH:
+				point.setY(point.getY() - 1);
+				break;
+			case EAST:
+				point.setX(point.getX() - 1);
+				break;
+			case WEST:
+				point.setX(point.getX() + 1);
+				break;
+			}
+		}
+	}
+
+	private void moveDirection(int command, Direction direction) {
+		if (command == 3) {
+			switch (direction) {
+			case NORTH:
+				point.setDirection(Direction.EAST);
+				break;
+			case SOUTH:
+				point.setDirection(Direction.WEST);
+				break;
+			case EAST:
+				point.setDirection(Direction.SOUTH);
+				break;
+			case WEST:
+				point.setDirection(Direction.NORTH);
+				break;
+			}
+		} else if (command == 4) {
+			switch (direction) {
+			case NORTH:
+				point.setDirection(Direction.WEST);
+				break;
+			case SOUTH:
+				point.setDirection(Direction.EAST);
+				break;
+			case EAST:
+				point.setDirection(Direction.NORTH);
+				break;
+			case WEST:
+				point.setDirection(Direction.SOUTH);
+				break;
+			}
+		}
+	}
+
 	private boolean checkIfPointIsOnTheTable() {
-		if(!PathValidator.validatePath(table, point)) {
+		if (!PathValidator.validatePath(table, point)) {
 			point.setX(-1);
 			point.setY(-1);
 			return false;
